d9e357a768fdfc1b42a49c7e6685e13789516362
Ammar942 <68778901+Ammar942@users.noreply.github.com>
2023-11-27 00:34:38 +0200

    added

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div className="hero">
       <div className="card text-bg-dark text-white border-0">
         <img
-          src="/assets/bg.jpg"
+          src="https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlnaHRpbmd8ZW58MHx8MHx8fDA%3D"
           class="card-img"
           alt="background"
           hright="550px"
